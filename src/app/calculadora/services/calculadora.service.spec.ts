0193cb80ffed8d05d756e4bdd5477bb27f3f3792
Erika Raissa <erikaaraissaqwe@gmail.com>
2021-07-05 22:08:17 -0300

    Implementação do Service e seus testes

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,40 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be  added correctly',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let sum = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(sum).toEqual(5);
+    })
+  );
+  
+  it('should be sub correctly',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let sub = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(sub).toEqual(-3);
+    })
+  );
+
+  it('should be multiplication correctly',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let mult = service.calcular(5, 5, CalculadoraService.MULTIPLICACAO);
+      expect(mult).toEqual(25);
+    })
+  );
+  
+  it('should be division correctly',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let div = service.calcular(40, 2, CalculadoraService.DIVISAO);
+      expect(div).toEqual(20);
+    })
+  );
+
+  it('should be return 0',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let error = service.calcular(40, 2, '.');
+      expect(error).toEqual(0);
+    })
+  );
+
 });
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,53 @@
+/**
+ *Serviço responsável por executar as operações da calculadora
+ * @author Erika Raissa Bueno da Silva
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
 
   constructor() { }
+
+  /**
+    * Esse método faz calculo utilizando as quatro operações matemáticas básica
+    *
+    * 
+    * 
+    * @param num1 number
+    * @param num2 number
+    * @param operacao string
+    * @return number 
+  */
+
+  calcular(num1:number, num2:number, operacao:string): number {
+
+    let resultado: number;
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
+
 }
