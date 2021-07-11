cd2d2e9a8842e742fadba4b106aeda37cdee1b81
Erika Raissa <erikaaraissaqwe@gmail.com>
2021-07-11 10:53:01 -0300

    Finalização do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CalculadoraModule } from './calculadora';
@@ -9,7 +10,8 @@ import { CalculadoraModule } from './calculadora';
     AppComponent
   ],
   imports: [
-    BrowserModule,  
+    BrowserModule,
+    FormsModule,
     CalculadoraModule
   ],
   providers: [],
--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -9,9 +9,127 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit {
 
+  private valor1: string;
+  private valor2: string;
+  private result: number;
+  private op: string;
+
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
+    this.limpar();
   }
+
+  /**
+   * Inicializa todos os campos com os valores padrão.
+   * 
+   * @return void
+  */
+  limpar() {
+    this.valor1 = '0';
+    this.valor2 = null;
+    this.result = null;
+    this.op = null;
+  }
+
+   /**
+   * Seta os valores já concatenados
+   * 
+   * 
+   * @param numero string
+   * @return void
+  */
+  adicionarNumero(numero: string): void{
+    if (this.op === null){
+      this.valor1 = this.concatenarNumero(this.valor1, numero);
+    }else{
+      this.valor2 = this.concatenarNumero(this.valor2, numero);
+    }
+  }
+
+  /**
+   * Faz o tratamento de decimal e concatena os numeros
+   * 
+   * 
+   * @param numAtual string
+   * @param numConcat string
+   * @return string
+  */
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if(numAtual === '0' || numAtual === null){
+      numAtual = '';
+    }
+
+    if(numConcat === '.' && numAtual === ''){
+      return '0.'
+    }
+
+    if(numConcat === '.' && numAtual.indexOf('.')> -1){
+      return numAtual;
+    }
+    let valorFinal = numAtual + numConcat;
+    return valorFinal;
+  }
+
+  /**
+   * Define a operação a ser utilizada
+   * 
+   * 
+   * @param op string
+   * @return string
+  */
+ defineOperacao(op: string): void{
+   if(this.op === null){
+     this.op = op;
+     return;
+   }
+
+   if(this.valor2 !== null){
+     this.result = this.calculadoraService.calcular(
+       parseFloat(this.valor1),
+       parseFloat(this.valor2),
+       this.op);
+    
+      this.op = op;
+      this.valor1 = this.result.toString();
+      this.valor2 = null;
+      this.result = null;
+   }
+  }
+
+   /**
+   * Efetua o cálculo
+   * 
+   * 
+   * @return void
+  */
+ calcular(): void{
+   
+  if(this.valor2 === null){
+    return;
+  }
+
+  this.result = this.calculadoraService.calcular(
+    parseFloat(this.valor1),
+    parseFloat(this.valor2),
+    this.op);
+
+ }
+
+/**
+   * Mostra no display os valores
+   * 
+   * 
+   * @return string
+  */
+ get display(): string{
+   if(this.result !== null){
+     return this.result.toString();
+   }
+   if(this.valor2 !== null){
+     return this.valor2;
+   }
+    return this.valor1;
+ }
 
 }
